a8319e0aa5af760db0929201645a2ce90f12cf14
Julian Andrade <julian@ndrade.com.br>
2017-10-18 17:27:12 -0200

    Estilo acordeão para o filtro de tipo de post, nas páginas de loop.

--- template/filter-tipo.php
+++ template/filter-tipo.php
@@ -1,25 +1,37 @@
-<form id='tipo-form' method='get'>
+<?php
+ob_start();
+?><form id='tipo-form' method='get'>
 	<input type='hidden' name="tipo" />
-<?php
-$submitted_tipo = array();
+	<?php
+	$submitted_tipo = array();
 
-if(filter_has_var(INPUT_GET, 'tipo')) {
-	$submitted_tipo = explode(',', filter_input(INPUT_GET, 'tipo'));
-}
+	if (filter_has_var(INPUT_GET, 'tipo')) {
+		$submitted_tipo = explode(',', filter_input(INPUT_GET, 'tipo'));
+	}
 
-$tipo_postagem = array (
-	'lex' => 'Lex',
-	'noticia' => 'Notícia',
-	'artigo' => 'Artigo',
-	'materia' => 'Matéria',
-	'informacao' => 'Informação',
-	'jurisprudencia' => 'Jurisprudência',
-);
+	$tipo_postagem = array(
+		'lex' => 'Lex',
+		'noticia' => 'Notícia',
+		'artigo' => 'Artigo',
+		'materia' => 'Matéria',
+		'informacao' => 'Informação',
+		'jurisprudencia' => 'Jurisprudência',
+	);
 
-foreach($tipo_postagem as $key=>$label) { 
-	$checked = (in_array($key, $submitted_tipo) ? 'checked' : null) ?>
-	<label><input type="checkbox" value="<?php echo $key ?>" <?php echo $checked ?>> <?php echo $label ?></label>
-<?php
-} ?>
+	foreach ($tipo_postagem as $key => $label) {
+		$checked = (in_array($key, $submitted_tipo) ? 'checked' : null) ?>
+		<label><input type="checkbox" value="<?php echo $key ?>" <?php echo $checked ?>> <?php echo $label ?></label>
+		<?php }
+	?>
 	<input type='button' value='Filtrar' id='tipo-submit' />
-</form>
+</form><?php
+$form_content = ob_get_clean();
+
+$accordion = '[vc_tta_accordion active_section="null" collapsible_all="true"][vc_tta_section title="Filtrar por tipo" tab_id="1508348492992-eb970e03-2378"]'
+		. $form_content
+		. '[/vc_tta_section][/vc_tta_accordion]';
+$filtered_accordion = apply_filters( 'the_content', $accordion); ?>
+
+<div class="col-xl-12 un-page-content-inner un-text-color filter-tipo-container">
+	<?php echo $filtered_accordion ?>
+</div>
