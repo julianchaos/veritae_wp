71341630d4f437188d73d9d70a83b1b09c60671d
Julian Andrade <julian@ndrade.com.br>
2017-12-17 20:58:21 -0200

    Filtro para definição de data início e limite para as postagens listadas, no VOE.

---
+++ header-voe.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Description: The Header
+Theme: Maya Reloaded
+*/
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <title><?php wp_title('', true); ?></title>
+    
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <?php wp_head(); ?>
+    
+</head>
+
+<body <?php body_class(); ?>>
--- template/voe.php
+++ template/voe.php
@@ -3,53 +3,93 @@
  * Template Name: VOE
  */
 
-$date = strtotime("+1 day");
-$args = array(
-	'post_type' => 'post',
-	'date_query' => array(
-		'day' => date('d', $date),
-		'month' => date('m', $date),
-		'year' => date('Y', $date)
-	),
-);
+get_header('voe') ?>
 
-$query = new WP_Query();
-do {
-	$date = strtotime("-1 day", $date);
-	$args['date_query']['day'] = date('d', $date);
-	$args['date_query']['month'] = date('m', $date);
-	$args['date_query']['year'] = date('Y', $date);
+<form method="get">
+	<label>
+		A partir de <small>(inclusive)</small>
+		<input type="date" name="after" required />
+	</label>
 	
-	$query->query($args);
+	<label>
+		Até <small>(inclusive)</small>
+		<input type="date" name="before" required />
+	</label>
+		
+	<input type="submit" value="Filtrar" class="btn-filter" />
+</form>
+
+<?php
+
+if(filter_has_var(INPUT_GET, 'start')) {
+	$start = filter_input(INPUT_GET, 'start');
+	$end = filter_input(INPUT_GET, 'end');
 	
-} while (!$query->have_posts());
+	$args = array(
+		'date_query' => array(
+			'after' => $start,
+			'before' => $end,
+			'inclusive' => true
+		)
+	);
+	$query = new WP_Query($args);
+}
+else {
+	$date = strtotime("+1 day");
+	$args = array(
+		'post_type' => 'post',
+		'date_query' => array(
+			'day' => date('d', $date),
+			'month' => date('m', $date),
+			'year' => date('Y', $date)
+		),
+	);
+
+	$query = new WP_Query();
+	do {
+		$date = strtotime("-1 day", $date);
+		$args['date_query']['day'] = date('d', $date);
+		$args['date_query']['month'] = date('m', $date);
+		$args['date_query']['year'] = date('Y', $date);
+
+		$query->query($args);
+
+	} while (!$query->have_posts());
+	
+}
+
 
 
 $output = array();
-while($query->have_posts()) {
-	$query->the_post();
-	
-	$tipo_postagem = get_post_meta($post->ID, 'tipo_postagem', true);
-	if(!array_key_exists($tipo_postagem, $output)) {
-		$output[$tipo_postagem] = array();
+if($query->have_posts()) {
+	while($query->have_posts()) {
+		$query->the_post();
+
+		$tipo_postagem = get_post_meta($post->ID, 'tipo_postagem', true);
+		if(!array_key_exists($tipo_postagem, $output)) {
+			$output[$tipo_postagem] = array();
+		}
+
+		$area_conhecimento = wp_get_post_terms($post->ID, 'area_conhecimento');
+		$area_conhecimento_title = 'GERAL';
+		if(
+				is_array($area_conhecimento) && 
+				count($area_conhecimento) > 0) {
+			$area_conhecimento_title = $area_conhecimento[0]->name;
+		}
+
+		if(!array_key_exists($area_conhecimento_title, $output[$tipo_postagem])) {
+			$output[$tipo_postagem][$area_conhecimento_title] = array();
+		}
+
+		$output[$tipo_postagem][$area_conhecimento_title][] = array(
+			'title' => get_the_title(),
+			'link' => get_the_permalink(),
+		);
 	}
-	
-	$area_conhecimento = wp_get_post_terms($post->ID, 'area_conhecimento');
-	$area_conhecimento_title = 'GERAL';
-	if(
-			is_array($area_conhecimento) && 
-			count($area_conhecimento) > 0) {
-		$area_conhecimento_title = $area_conhecimento[0]->name;
-	}
-	
-	if(!array_key_exists($area_conhecimento_title, $output[$tipo_postagem])) {
-		$output[$tipo_postagem][$area_conhecimento_title] = array();
-	}
-	
-	$output[$tipo_postagem][$area_conhecimento_title][] = array(
-		'title' => get_the_title(),
-		'link' => get_the_permalink(),
-	);
+}
+else {
+	echo "<h1>Não existem postagens no período definido</h1>";
 }
 
 foreach($output as $tipo => $area_arr) {
@@ -66,3 +106,5 @@ foreach($output as $tipo => $area_arr) {
 		}
 	}
 }
+
+get_footer('voe');
