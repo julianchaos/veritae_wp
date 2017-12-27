b2d45f02c3ce98cda48e5bb3441dfad14c01ecc8
Julian Andrade <julian@ndrade.com.br>
2017-12-27 14:06:33 -0200

    Seta "chevron-down" na página inicial, indicando que há mais conteúdo além da visualização inicial.

--- functions.php
+++ functions.php
@@ -3,8 +3,7 @@
 <?php require_once 'assets/inc/cpf.php' ?>
 <?php
 
-function veritae_theme_enqueue_styles()
-{
+function veritae_theme_enqueue_styles() {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 	
@@ -14,6 +13,10 @@ add_action('wp_enqueue_scripts', 'veritae_theme_enqueue_styles');
 
 function veritae_theme_enqueue_scripts() {
 	wp_enqueue_script('veritae-main', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'));
+	
+	if(is_home()) {
+		wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/55e074484c.js');
+	}
 } 
 add_action('wp_enqueue_scripts', 'veritae_theme_enqueue_scripts');
 
