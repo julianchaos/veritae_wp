dc57c3b673ddf25990c68cda7bd2232e6ff4fccf
Julian Andrade <julian@ndrade.com.br>
2018-01-28 19:59:17 -0200

    Botão "Limpar Filtro"

--- template/filter-tipo.php
+++ template/filter-tipo.php
@@ -5,13 +5,26 @@ ob_start();
 	if(filter_has_var(INPUT_GET, 's')) { ?>
 	<input type='hidden' name='s' value='<?php echo filter_input(INPUT_GET, 's') ?>' />
 <?php
-	} ?>
+	}
+	
+	if(filter_has_var(INPUT_GET, 'voe')) { ?>
+	<input type='hidden' name='voe' value='<?php echo filter_input(INPUT_GET, 'voe') ?>' />
+<?php
+	}
+	?>
 	<input type='hidden' name="tipo" />
 	<?php
 	$submitted_tipo = array();
-
+	$clear_filter = null;
+	
 	if (filter_has_var(INPUT_GET, 'tipo')) {
 		$submitted_tipo = explode(',', filter_input(INPUT_GET, 'tipo'));
+		
+		$query_data = filter_input_array(INPUT_GET);
+		unset($query_data['tipo']);
+		
+		$clear_filter = explode("?", $_SERVER['HTTP_REFERER'])[0];
+		$clear_filter .= "?" . http_build_query($query_data);
 	}
 
 	$tipo_postagem = array(
@@ -37,6 +50,11 @@ $accordion = '[vc_tta_accordion active_section="null" collapsible_all="true"][vc
 		. '[/vc_tta_section][/vc_tta_accordion]';
 $filtered_accordion = apply_filters( 'the_content', $accordion); ?>
 
-<div class="col-xl-12 un-page-content-inner un-text-color filter-tipo-container">
-	<?php echo $filtered_accordion ?>
+<div class="col-xl-12 un-page-content-inner un-text-color filter-container filter-tipo-container">
+	<?php
+	echo $filtered_accordion;
+	
+	if(filter_has_var(INPUT_GET, 'tipo')) {
+		echo "<a href='$clear_filter' class='clear-filter'>Limpar Filtro</a>";
+	} ?>
 </div>
